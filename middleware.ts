0f923b32fad2781a7d64cdf7ba25f2f681b3c702
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-11-03 12:06:03 +0330

    Fix: fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,27 @@
 /* eslint-disable */
-
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerCookie } from "./helper/server-cookie";
 import { verifyToken } from "./lib/verifyToken";
 
 export async function middleware(req: NextRequest) {
   const token = await getServerCookie("access_token");
   const verified: any = await verifyToken(token);
-
   const { pathname } = req.nextUrl;
 
-  if (verified !== null && pathname.startsWith("/login")) {
+  const isAuthPage =
+    pathname.startsWith("/login") || pathname.startsWith("/register");
+  const isDashboardPage = pathname.startsWith("/dashboard");
+
+  if (!verified && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (verified && isAuthPage) {
     return NextResponse.redirect(new URL("/", req.url));
   }
-  if (!verified && pathname == "/") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  if (
-    verified &&
-    !verified &&
-    verified.role !== "ADMIN" &&
-    pathname.startsWith("/dashboard")
-  ) {
-    return NextResponse.redirect(new URL("/login", req.url));
+
+  if (verified && verified.role !== "ADMIN" && isDashboardPage) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
